ddfd9cfd92a1a19e874b4fa97e4f120c308084ea
Carlos <carlos.edu.tec@hotmail.com>
2015-10-05 01:57:43 -0300

    primeiro teste

---
+++ connect_bd.php
@@ -0,0 +1,20 @@
+﻿<?php
+$servername = "127.0.0.1";
+$username = "username";
+$password = "";
+$dbname = "painel";
+
+// Cria conexão
+$conn = @mysqli_connect($servername, $username, $password, $dbname);
+// Verifica conexão
+echo "Status da conexão: ";
+if (!$conn) {
+    die("Falha na conexão: " . mysqli_connect_error());
+
+}else{
+	echo"<hr>";
+}
+?>
+
+
+
